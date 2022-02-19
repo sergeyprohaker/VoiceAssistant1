afed21844947b0db533b0e04b9c81d73a7d0b656
sergeyprohaker <42sdn@mail.ru>
2022-02-19 23:01:14 +0700

    Added opening of default apps

---
+++ voiceAssistant/startapp.py
@@ -0,0 +1,17 @@
+import os
+import funcs
+
+
+def startapp():
+    try:
+        if funcs.voice.split()[-1] == 'блокнот':
+            app_name = ' notepad'
+        elif funcs.voice.split()[-1] == 'рисования':
+            app_name = ' mspaint'
+        elif funcs.voice.split()[-1] == 'задач':
+            app_name = ' taskmgr'
+        elif funcs.voice.split()[-1] == 'проводник':
+            app_name = ' explorer'
+        os.system('start' + app_name)
+    except NameError:
+        print('Не удалось найти стандартную программу')
